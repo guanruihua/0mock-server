7bc08b4692560cae8bce833c4758d57da180e538
grh-gitee <3494242908@qq.com>
2022-10-16 11:46:22 +0800

    refactor: sentence

--- src/controllers/sentence.ts
+++ src/controllers/sentence.ts
@@ -5,7 +5,7 @@ import { loadApis } from '../lib'
 
 // eslint-disable-next-line
 export function sentence(param: Record<string, any>, req: Request, res: Response): Record<string, any> {
-	let lorem: any = (new LoremIpsum()).generateSentences(Math.min(parseInt(req.params[0]), MAX_SENTENCE))
+	const lorem: any = (new LoremIpsum()).generateSentences(Math.min(parseInt(req.params[0]), MAX_SENTENCE))
 	if (req.params[1] === ',') {
 		return lorem
 			.split('.')
